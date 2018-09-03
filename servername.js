d0015dd045b159cbc59218706c35fb3c4d90083a
oonishin <oonishin@gmail.com>
2018-09-03 17:52:32 +0900

    Update servername.js

--- servername.js
+++ servername.js
@@ -4,7 +4,9 @@ $.ajax({
   cache   : false,
   success : function (data, status, xhr) {
     var serverName = xhr.getResponseHeader('Server');
-    console.log(serverName);
-    $("#imui-container > div.imui-title > h1 ").text("MyBox: " + serverName)
+    //console.log(serverName);
+    //$("#imui-container > div.imui-title > h1 ").text("MyBox: " + serverName)
+    $("#imui-nav-global > div > h1 > a > img").append('<div style=”position:absolute; top:50px; left:40px; width: 430px;”>' + serverName
+ + '</div>');      
   }
 });
