368e206f469deff5223b7132b5d3d607799f2168
oonishin <oonishin@gmail.com>
2018-09-03 17:56:55 +0900

    Update servername.js

--- servername.js
+++ servername.js
@@ -4,7 +4,7 @@ $.ajax({
   cache   : false,
   success : function (data, status, xhr) {
     var serverName = xhr.getResponseHeader('Server');
-    //console.log(serverName);
+    console.log(serverName);
     //$("#imui-container > div.imui-title > h1 ").text("MyBox: " + serverName)
     $("#imui-nav-global > div > h1 > a > img").append('<div style=”position:absolute; top:50px; left:40px; width: 430px;”>' + serverName
  + '</div>');      
