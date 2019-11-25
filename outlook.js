6f9798f40efa84ebd1957e2fd277e51b626aed89
oonishin <oonishin@gmail.com>
2019-11-26 01:20:19 +0900

    Update outlook.js

--- outlook.js
+++ outlook.js
@@ -1,6 +1,7 @@
 
 // 転送メール無効化通知を自動クローズ
 document.addEventListener("DOMContentLoaded", function() {
+  console.log('Event Fire!');
   var targetClass = 'ms-Button _23T7xmD47XsO8wwTa_w-ro ms-Button--icon _1HNQs-NWhKrh1Bs9Hh_iTS root-58';
   var targetElements = document.getElementsByClassName(targetClass);
   if (targetElements.length == 1){
