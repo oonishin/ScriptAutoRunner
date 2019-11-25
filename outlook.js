4588f4372365812ec578634b927cf52c710bab67
oonishin <oonishin@gmail.com>
2019-11-26 01:13:08 +0900

    Update outlook.js

--- outlook.js
+++ outlook.js
@@ -1,6 +1,8 @@
 // 転送メール無効化通知を自動クローズ
 var targetClass = 'ms-Button _23T7xmD47XsO8wwTa_w-ro ms-Button--icon _1HNQs-NWhKrh1Bs9Hh_iTS root-58';
 var targetElements = document.getElementsByClassName(targetClass);
-if (targetElements.length == 1){
-targetElements[0].click();
-}
+do{
+  if (targetElements.length == 1){
+    targetElements[0].click();
+  }
+}while (targetElements.length == 0)
