5fbae28687c2156310394e27760f6f745edcd61a
oonishin <oonishin@gmail.com>
2019-11-26 02:22:07 +0900

    Update outlook.js

--- outlook.js
+++ outlook.js
@@ -6,7 +6,10 @@ window.addEventListener('DOMContentLoaded', function(e){
   // 監視ターゲットの取得
   const targetClass = 'ms-Button _23T7xmD47XsO8wwTa_w-ro ms-Button--icon _1HNQs-NWhKrh1Bs9Hh_iTS root-58';
   const target = document.getElementsByClassName(targetClass);
-  
+  if (targetElements.length == 1){
+    targetElements[0].click();
+  }
+  /*
   // オブザーバーの作成
   const observer = new MutationObserver(records => {
     // 変化が発生したときの処理を記述
@@ -19,4 +22,5 @@ window.addEventListener('DOMContentLoaded', function(e){
     attributes: true,
     childList: true
   })
+*/
 }, {once: true});
