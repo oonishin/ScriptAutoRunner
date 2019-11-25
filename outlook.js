e52521711ac2b71ccd358dacfa61835e65b8ecb9
oonishin <oonishin@gmail.com>
2019-11-26 02:32:01 +0900

    Update outlook.js

--- outlook.js
+++ outlook.js
@@ -5,10 +5,10 @@ console.log('Event Load!');
     console.log('Event Fire!');
     // 監視ターゲットの取得
     const targetClass = 'ms-Button _23T7xmD47XsO8wwTa_w-ro ms-Button--icon _1HNQs-NWhKrh1Bs9Hh_iTS root-58';
-    const target = document.getElementsByClassName(targetClass);
+    const targetElements = document.getElementsByClassName(targetClass);
     if (targetElements.length == 1){
       targetElements[0].click();
     }
   }
 
-setTimeout(close, 5000);
+setTimeout(close, 3500);
