f3ef7e52cb5ba8890bd8914ca875a1f2a02226e0
oonishin <oonishin@gmail.com>
2019-11-26 02:41:43 +0900

    Update outlook.js

--- outlook.js
+++ outlook.js
@@ -1,5 +1,3 @@
-console.log('Event Load!');
-
 // 受信トレイの欄を消す
 var targetClass = '_3oZkxfK_sfMuqvT6pZ7cT9 R13rCAdCs2xraNrPX7bVq _3dW5_u_jpraXm7_TFtLx1F';
 var targetElements = document.getElementsByClassName(targetClass);
@@ -9,7 +7,7 @@ if (targetElements.length == 1){
 
 // 転送メール無効化通知を自動クローズ
   var close = function(){
-    console.log('Event Fire!');
+    //console.log('Event Fire!');
     // 監視ターゲットの取得
     const targetClass = 'ms-Button _23T7xmD47XsO8wwTa_w-ro ms-Button--icon _1HNQs-NWhKrh1Bs9Hh_iTS root-58';
     const targetElements = document.getElementsByClassName(targetClass);
