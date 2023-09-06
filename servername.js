e479fe8cedfbb9fb003c6c57d7747a7283eab1f5
oonishin <oonishin@gmail.com>
2023-09-06 10:33:30 +0900

    Update servername.js

--- servername.js
+++ servername.js
@@ -6,5 +6,6 @@ $.ajax({
     var serverName = xhr.getResponseHeader('Server');
     console.log(serverName);
     $("#imui-user-utility > li > a > span.ui-button-text").append('<br>' + serverName);
+    $("#im-element-4 > div > div.im-hichee-side-menu-utility > ul > li > span.im-hichee-side-menu-utility-user-name").append(' (' + serverName + ')');
   }
 });
