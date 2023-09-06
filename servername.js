6e29365c1472c11a5143a5ac59a866eb6b6d3ee0
oonishin <oonishin@gmail.com>
2023-09-06 10:46:39 +0900

    Update servername.js

--- servername.js
+++ servername.js
@@ -5,7 +5,7 @@ $.ajax({
   success : function (data, status, xhr) {
     var serverName = xhr.getResponseHeader('Server');
     console.log(serverName);
-    $("#imui-user-utility > li > a > span.ui-button-text").append('<br>' + serverName);
-    $("#im-element-4 > div > div.im-hichee-side-menu-utility > ul > li > span.im-hichee-side-menu-utility-user-name").append(' (' + serverName + ')');
+    $("#imui-user-utility > li > a > span.ui-button-text").append(' ( ' + serverName + ')');
+    #$("#im-element-4 > div > div.im-hichee-side-menu-utility > ul > li > span.im-hichee-side-menu-utility-user-name").append(' (' + serverName + ')');
   }
 });
